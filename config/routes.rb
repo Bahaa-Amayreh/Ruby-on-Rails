e81b51187a4dc3c4292c4336132872ffcdb00777
Baha Amayreh <amayreh.baha@gmail.com>
2025-11-08 10:02:03 -0500

    Add Rails app with configuration, models, views, and controllers

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # get 'users/new'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/baha'
+
+  get 'home', to: 'static_pages#home'
+   get 'help', to: 'static_pages#help'
+    get 'about', to: 'static_pages#about'
+     get 'baha', to: 'static_pages#baha'
+     get 'users/new', to: 'users#new', as: 'new_user'
+  
+end
