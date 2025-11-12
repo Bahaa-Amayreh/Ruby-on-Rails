e403f862608e5f63d6f041e2f8dec957ad21d019
Bahaa-Amayreh <amayreh.1@osu.edu>
2025-11-12 16:03:12 -0500

    Add user and post model ...

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_07_181209) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_12_182225) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "topic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
+    t.string "title"
   end
 
+  add_foreign_key "posts", "users"
 end
